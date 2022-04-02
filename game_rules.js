331339dbdfed7df59358c19e5363915af704df41
rileytm <riley.meskill@gmail.com>
2022-04-02 08:43:36 -0500

    creating repo

---
+++ game_rules.js
@@ -0,0 +1,19 @@
+let board = [   [1, 2, 3, 4, 5, 6], //the top of the board where chips go in
+                [1, 2, 3, 4, 5, 6], //so that the user selects the array 
+                [1, 2, 3, 4, 5, 6], //they're inserting a chip into
+                [1, 2, 3, 4, 5, 6], //and it "falls" into the spot in the array
+                [1, 2, 3, 4, 5, 6], //that is not occupied
+                [1, 2, 3, 4, 5, 6],
+                [1, 2, 3, 4, 5, 6]   ];
+
+function checkRow(){
+    //really checking same index across each sub-array (vertically above)
+}
+
+function checkColumn(){
+    //really checking each sub-array (horizontally above)
+}
+
+function checkDiag(){
+
+}
